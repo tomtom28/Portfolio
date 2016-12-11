cb31c08ab3f854627379bbaddaeb9059ef823784
tomtom28 <tomthompson828@gmail.com>
2016-12-10 21:39:35 -0500

    fixed the logo glitch for mobile. dropdown closes now

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,16 @@
-// Modal Escape Key Listener
 $(document).ready(function(){
 
+  // Modal Escape Key Listener
   $(document).keyup(function(e){
       if(e.which == 27){
           // Close my modal window
           $('.portfolio-modal').modal('hide');
       }
+  });
+
+  // Close the menu if the student logo is clicked
+  $('.navbar-brand').on('click', function(){
+    $('.navbar-toggle:visible').click();
   });
 
 });
